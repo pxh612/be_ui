5c92275ad1e89b70806e3e4b111ab4d18c7558f9
pxh612 <pxh612@gmail.com>
2023-04-21 17:55:46 +0700

    Problem: faded font color

--- app/src/main/java/com/pxh612/be_ui/MainActivity.kt
+++ app/src/main/java/com/pxh612/be_ui/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.pxh612.be_ui.databinding.ActivityMainBinding
@@ -15,9 +17,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initBottomNavigationView()
+    }
+
+    private fun initBottomNavigationView() {
+        /*val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mobile_navigation) as NavHostFragment
+        val navController = navHostFragment.navController*/
     }
 }
